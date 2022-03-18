f6da77c59c8f2f0d2f55386d9ade5dcba2ffff46
magnusgsvend@gmail.com <mgs@iedi.com>
2022-03-18 15:14:00 +0100

    awesome debug code removal

--- main.go
+++ main.go
@@ -109,7 +109,7 @@ func handleChallenge(chal Challenge, rootPath string) {
 }
 
 func recieveChallenges(conn *websocket.Conn) {
-	//defer close(done)
+	defer close(done)
 	_, challengesJSON, _ := conn.ReadMessage()
 	fmt.Printf("%s\n", challengesJSON)
 	/*
